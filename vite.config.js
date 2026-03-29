2e1258157ab7b2f174689c64040e023bc6bc25a4
ssukhraj12 <ssukhraj12@gmail.com>
2026-03-29 20:52:53 +0530

    vite package

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
     plugins: [
@@ -20,6 +21,7 @@ export default defineConfig({
                 },
             },
         }),
+        vuetify({ autoImport: true }),
     ],
     resolve: {
         alias: {
