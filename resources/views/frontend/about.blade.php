30d89f5ddc94a899262ab763c2085582904d266c
ssukhraj12 <ssukhraj12@gmail.com>
2026-04-06 15:03:45 +0530

    about and contact page

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -1,5 +1,111 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>About Page</h1>
+    @php
+        $glances = [
+             [
+                "title" => "Tracked delivery services are at our core.",
+                "icon" => "mdi-truck-fast",
+                "text" => "We pride ourselves on our service levels and the ability to meet tight delivery deadlines across the UK."
+             ],
+            [
+                "title" => "International reach with a local feel.",
+                "icon" => "mdi-earth",
+                "text" => "Our 26 strategically partners placed service centres are run by local delivery experts, ensuring we successfully deliver for our customers."
+            ],
+            [
+                "title" => "Award-winning customer service.",
+                "icon" => "mdi-shield-check",
+                "text" => "Our customer care teams are here to support you every step of the way, whenever you need us."
+            ],
+            [
+                "title" => "Building a partnership on trust.",
+                "icon" => "mdi-handshake",
+                "text" => "We’re here as an extension of your business to help you grow and succeed. You can trust us to deliver, it’s what we do best."
+            ],
+            [
+                "title" => "Here for the communities we serve.",
+                "icon" => "mdi-hand-heart-outline",
+                "text" => "From our sustainable initiatives to local charity support, to recruiting and developing people, we’re here for you."
+            ],
+            [
+                "title" => "Investing in innovation technology.",
+                "icon" => "mdi-code",
+                "text" => "Wherever technology can better your experience, improve efficiency or minimise our environmental impact, we invest in it."
+            ]
+    ];
+    $avals = [
+             [
+                "title" => "Honesty.",
+                "icon" => "mdi-truck-fast",
+                "text" => "We value facts and measure our performance with simple visible metrics. Integrity and transparency are at the core of everything that we do."
+             ],
+            [
+                "title" => "Respect.",
+                "icon" => "mdi-earth",
+                "text" => "We create a welcoming, collaborative and inclusive culture. We value debate and encourage feedback from everyone."
+            ],
+            [
+                "title" => "Accountability.",
+                "icon" => "mdi-shield-check",
+                "text" => "We take personal accountability to deliver what matters every day. We have clear ownership of our goals, and we make the right things happen."
+            ],
+            [
+                "title" => "Caring.",
+                "icon" => "mdi-handshake",
+                "text" => "We take the time to listen and understand what’s important. We are compassionate and show empathy. We commit to doing business safely and sustainably."
+            ],
+            [
+                "title" => "Passion.",
+                "icon" => "mdi-hand-heart-outline",
+                "text" => "We celebrate our achievements and recognise our people through our values, believing in ourselves and our teams. We never stop progressing and shaping the delivery service of tomorrow."
+            ],
+            [
+                "title" => "Flexibility.",
+                "icon" => "mdi-code",
+                "text" => "We embrace change, challenging ourselves to move quickly, be adaptable and take smart risks. We constantly challenge the status quo to ensure that the business remains flexible and agile."
+            ]
+    ]
+    @endphp
+    <section class="sheader bg-primary text-white text-shadow-xs text-shadow-gray-400 py-10">
+        <div class="container max-w-7xl flex flex-col gap-2 items-center text-center">
+            <h1 class="text-3xl lg:text-5xl">About Courier City Ltd.</h1>
+            <h2 class="text-2xl lg:text-4xl mb-4">Delivering what matters for our community.</h2>
+            <p class="text-xl">Supporting customers, colleagues and suppliers to grow and be successful.</p>
+        </div>
+    </section>
+    <section class="py-12 bg-linear-to-tr from-orange-200 via-white/50 to-primary/50">
+        <div class="container max-w-7xl">
+            <h2>Courier City at a glance.</h2>
+            <p>
+                At Courier City, we’re clear on who we are and why we exist. Our purpose is simple: “Delivering what matters.” We are passionate about delivering what matters for our customers, colleagues and suppliers. Whether we are delivering life-saving equipment or a parcel on behalf of a local business, we deliver what matters.
+            </p>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-6">
+                @foreach($glances as $glance)
+                    <div class="shadow-xl p-2 lg:p-4 bg-white rounded-xl overflow-hidden hover:-translate-y-1 transition-all border border-gray-200">
+                        <i class="iconify text-4xl" data-icon="{{$glance['icon']}}"></i>
+                        <h3 class="text-primary">{{$glance['title']}}</h3>
+                        <p>{{$glance['text']}}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+    <section class="py-12 bg-linear-to-br from-orange-200 via-white/50 to-primary/50">
+        <div class="container max-w-7xl">
+            <h2>Our Values.</h2>
+            <p>
+                How we'll do it.
+            </p>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-6">
+                @foreach($avals as $aval)
+                    <div class="shadow-xl p-2 lg:p-4 bg-white rounded-xl overflow-hidden hover:-translate-y-1 transition-all border border-orange-200">
+                        <i class="iconify text-6xl px-1 bg-primary rounded-xl text-white" data-icon="{{$aval['icon']}}"></i>
+                        <h3 class="text-secondary">{{$aval['title']}}</h3>
+                        <p class="text-sm">{{$aval['text']}}</p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,5 +1,106 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Contact Page</h1>
+    <section class="sheader bg-primary text-white text-shadow-xs text-shadow-gray-400 py-10">
+        <div class="container max-w-7xl flex flex-col gap-2 items-center text-center">
+            <h1 class="text-3xl lg:text-5xl">Contact Courier City Ltd.</h1>
+            <h2 class="text-2xl lg:text-4xl mb-4">Get In Touch.</h2>
+            <p class="text-xl">We're on hand to help, 24/7, 365 days a year.</p>
+        </div>
+    </section>
+    <section class="py-12 bg-linear-to-b lg:bg-linear-to-r from-secondary to-primary">
+        <div class="container max-w-7xl">
+            <h2 class="text-gray-50">How can we help?</h2>
+            <p class="text-gray-100">
+                Let us know how we can help by completing the form below. While we aim to respond as quickly as possible, please allow up to 24 hours for a reply (Monday to Friday).
+            </p>
+            <p class="text-gray-100">
+                If your query is urgent, or relates to a booking currently in progress, please contact your local service centre team. Find the contact details for your nearest location
+            </p>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-6">
+                <div class="flex flex-col gap-4">
+                    <div>
+                        <h5 class="font-bold text-primary uppercase tracking-wider text-base">Southall</h5>
+                        <h5 class="font-bold text-gray-200 mb-6 uppercase tracking-wider text-xs">Opp Fire Station</h5>
+                        <ul class="space-y-4 text-sm text-slate-300">
+                            <li class="flex flex-col gap-1">
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-map-marker" class="iconify text-primary"></i>
+                                    <span class="text-sm">32 High Street, Southall, UB1 3DA</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-phone" class="iconify text-primary"></i>
+                                    <span class="text-sm">0208 571 9555</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-whatsapp" class="iconify text-primary"></i>
+                                    <span class="text-sm">0755 381 2222</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-email" class="iconify text-primary"></i>
+                                    <span class="text-sm">couriercityuk@gmail.com</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 class="font-bold text-primary uppercase tracking-wider text-base">Southall</h5>
+                        <h5 class="font-bold text-gray-200 mb-6 uppercase tracking-wider text-xs">Near Railway Station</h5>
+                        <ul class="space-y-4 text-sm text-slate-300">
+                            <li class="flex flex-col gap-1">
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-map-marker" class="iconify text-primary"></i>
+                                    <span class="text-sm">12 Beaconsfield Road, Southall, UB1 1DW</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-phone" class="iconify text-primary"></i>
+                                    <span class="text-sm">0208 843 0092</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-whatsapp" class="iconify text-primary"></i>
+                                    <span class="text-sm">0788 833 8810</span>
+                                </div>
+                                <div class="flex items-center gap-1">
+                                    <i data-icon="mdi-email" class="iconify text-primary"></i>
+                                    <span class="text-sm">couriercityuk@gmail.com</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-3 bg-white p-2 lg:p-4 shadow rounded-xl overflow-hidden">
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
+                        <div>
+                            <label for="fname" class="block mb-1 text-sm font-medium">First Name</label>
+                            <input placeholder="First Name" name="fname" type="text" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="lname" class="block mb-1 text-sm font-medium">Last Name</label>
+                            <input placeholder="Last Name" name="lname" type="text" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-1 lg:grid-cols-2 gap-3">
+                        <div>
+                            <label for="cname" class="block mb-1 text-sm font-medium">Company Name</label>
+                            <input placeholder="Company Name" name="cname" type="text" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        </div>
+                        <div>
+                            <label for="email" class="block mb-1 text-sm font-medium">Email Address</label>
+                            <input placeholder="Email Address" name="email" type="text" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                        </div>
+                    </div>
+                    <div>
+                        <label for="phone" class="block mb-1 text-sm font-medium">Phone No</label>
+                        <input placeholder="Phone no" name="phone" type="text" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"/>
+                    </div>
+                    <div>
+                        <label for="message" class="block mb-1 text-sm font-medium">Message</label>
+                        <textarea rows="5" placeholder="type your message" name="message" class="w-full rounded-lg border border-gray-300 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        </textarea>
+                    </div>
+                    <div type="submit" title="Send Message" class="inline-flex px-4 py-1.5 rounded-lg text-white bg-primary justify-center text-shadow-xs text-shadow-gray-400 tracking-wide">Send Message</div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
