9bb00dbca6d097f07152f06c37d15dd708ebc55b
ssukhraj12 <ssukhraj12@gmail.com>
2026-05-02 15:14:29 +0530

    dpd logos and dashbaord link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,23 +74,27 @@
                         {{--                                </li>--}}
                         {{--                            @endif--}}
                     @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
+                        <a class="text-sm bg-primary inline-flex items-center gap-1 text-white font-bold capitalize px-4 py-1.5 rounded-full shadow hover:bg-secondary justify-center"
+                           href="/shipment/new">
+                            Book Parcel <i class="iconify" data-icon="mdi-package-variant-plus"></i>
+                        </a>
+{{--                        <li class="nav-item dropdown">--}}
+{{--                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
+{{--                                {{ Auth::user()->name }}--}}
+{{--                            </a>--}}
 
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
+{{--                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">--}}
+{{--                                <a class="dropdown-item" href="{{ route('logout') }}"--}}
+{{--                                   onclick="event.preventDefault();--}}
+{{--                                                 document.getElementById('logout-form').submit();">--}}
+{{--                                    {{ __('Logout') }}--}}
+{{--                                </a>--}}
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+{{--                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">--}}
+{{--                                    @csrf--}}
+{{--                                </form>--}}
+{{--                            </div>--}}
+{{--                        </li>--}}
                     @endguest
                 </nav>
 {{--                <div class="collapse lg:visible flex w-full gap-3">--}}
